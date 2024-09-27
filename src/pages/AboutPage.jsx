9ef5728c66d08b4b0d5d7cdf316190655a213c83
schneibley <Schneibley@gmail.com>
2024-09-27 10:04:17 -0400

    About additions

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,8 +4,10 @@ import './css/about.css';
 function AboutPage() {
   return (
     <div className="about">
-      <img src="your-photo-url" alt="Your Name" />
-      <p>Your short bio here...</p>
+      <img src="/DSC01586.jpg" alt="Your Name" />
+      <p>I have spent most of my adult life dedicating my time to practicing Brazilian Jiujitsu, but as I get older my time in competition is drawing to and end.  
+        Because of this I am taking it upon myself to build some skills in the world of coding so I have another outlet for my creativity and so I can open a gym of my own.
+      </p>
     </div>
   );
 }
