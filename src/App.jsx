21e4ca8743b1cce09b54f63e774be2d51166325d
schneibley <Schneibley@gmail.com>
2024-09-30 12:07:25 -0400

    refactored app and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import Portfolio from './pages/Portfolio';
-import ResumePage from './pages/ResumePage';
-import './index.css'
+import './index.css';
 
 function App() {
   return (
-    <Router>
+    <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<AboutPage />} />
-        <Route path="/projects" element={<Portfolio />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/resume" element={<ResumePage />} />
-      </Routes>
+      <main>
+        <Outlet /> {/* This renders the matched child route */}
+      </main>
       <Footer />
-    </Router>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import './index.css'; 
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import Portfolio from './pages/Portfolio';
+import ResumePage from './pages/ResumePage';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<AboutPage />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
